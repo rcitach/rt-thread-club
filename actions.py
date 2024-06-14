fbe1ae1bf56f911c40725ef4e85106a8b57e6374
rcitachi <yearsince@yeah.net>
2024-06-14 09:55:39 +0800

    update

--- actions.py
+++ actions.py
@@ -34,10 +34,8 @@ def main():
     password = os.environ["CLUB_PASSWORD"]
 
     execute_command("sudo apt -y update && apt -y upgrade > /dev/null 2>&1")
-    execute_command("sudo chmod a+x chromedriver > /dev/null 2>&1")
     execute_command("sudo apt install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils > /dev/null 2>&1")
-    execute_command("sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1")
-    execute_command("sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1; apt-get -fy install > /dev/null 2>&1")
+    execute_command("sudo apt-get install google-chrome-stable > /dev/null 2>&1")
     day_num = login_in_club(username, password)
     try:
         with open("sign_in_days.txt", "w") as f:
